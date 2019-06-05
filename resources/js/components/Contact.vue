4aef78d83426966fe4c2be7522a1b9de07375eef
Anastasiia <anastasiiadev@gmail.com>
2019-06-05 22:53:35 +0700

    Style changes on the Contact page

--- resources/js/components/Contact.vue
+++ resources/js/components/Contact.vue
@@ -1,12 +1,12 @@
 <template>
 	<div class="output">
-		<p><a href="">My email</a></p>
+		<p>
+			<a href="">My email</a>
+		</p>
 
-		<br />
-
-		<p><a href="" download>Download my CV</a></p>
-
-		<br />
+		<p>
+			<a href="" download>Download my CV</a>
+		</p>
 
 	</div>
 </template>
